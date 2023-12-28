59906cefbfff7f474ad15603517ab2e7f1c5a078
Shantanu Mahale <46351765+shantanubr@users.noreply.github.com>
2023-12-29 00:56:13 +0530

    micro frontend for x

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [secretCode, setSecretCode] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const [tweetString, setTweetString] = useState<string>("");
+  const [response, setResponse] = useState(null);
+
+  const handleGetIn = () => {
+    if (secretCode !== process.env.REACT_APP_SECRET_CODE) {
+      setError(true);
+    } else {
+      setError(false);
+      setIsAuthenticated(true);
+    }
+  };
+
+  const sendTweet = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/twitter-api/tweets", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "YourTwitterAuthorizationHeaderHere",
+        },
+        body: JSON.stringify({ text: tweetString }),
+      });
+
+      const data = await response.json();
+      setResponse(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col h-screen w-screen items-center justify-center bg-[#202020]">
+      {isAuthenticated ? (
+        <div className="text-white flex flex-col">
+          <p>Tweet here...</p>
+          <textarea
+            value={tweetString}
+            onChange={(e) => setTweetString(e.target.value)}
+            className="bg-[#151515] mt-2 w-full"
+          />
+          <button
+            onClick={sendTweet}
+            className="border mt-4 text-white p-1 px-2 rounded-md">
+            SEND TWEET
+          </button>
+          {response && (
+            <div>
+              <h2>Server Response:</h2>
+              <pre>{JSON.stringify(response, null, 2)}</pre>
+            </div>
+          )}
+        </div>
+      ) : (
+        <>
+          {error && (
+            <div>
+              <p className="text-rose-600">ERROR!</p>
+            </div>
+          )}
+          <input
+            type="text"
+            value={secretCode}
+            onChange={(e) => setSecretCode(e.target.value)}
+          />
+          <button
+            onClick={handleGetIn}
+            className="border mt-4 text-white p-1 px-2 rounded-md">
+            GET IN
+          </button>
+        </>
+      )}
     </div>
   );
 }
